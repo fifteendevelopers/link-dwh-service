fea78b784f3c5061c7e35171302bc6d5e5714e69
fifteendevelopers <developers@fifteen.co.uk>
2026-06-05 14:14:11 +0100

    Web routes added to enable redirection and noindex

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * instruct legitimate web crawlers (Google, Bing, etc.) not to index the domain.
+ */
+Route::get('/robots.txt', function () {
+    return response("User-agent: *\nDisallow: /", 200)
+        ->header('Content-Type', 'text/plain');
 });
+
+/**
+ * Redirect all but API requests to the main bikeability website
+ */
+Route::any('{any}', function () {
+    return redirect('https://bikeability.org.uk', 301); // 301 = Permanent Redirect
+})->where('any', '^(?!api).*$');
