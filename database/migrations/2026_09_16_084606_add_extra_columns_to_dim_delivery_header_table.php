4229422f2b7cacce2c74080ee5b05e1cfcfed5b4
fifteendevelopers <developers@fifteen.co.uk>
2026-09-16 10:48:14 +0100

    DRF-105 update Dim Delivery Header with additional data columns

--- database/migrations/2026_09_16_084606_add_extra_columns_to_dim_delivery_header_table.php
+++ database/migrations/2026_09_16_084606_add_extra_columns_to_dim_delivery_header_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('Dim_Delivery_Header', function (Blueprint $table) {
             // Legacy & Alternative Grant Identifiers
             $table->string('Short_Description', 10)->nullable()->after('Delivery_Status');
-            $table->unsignedInteger('Local_Funding')->default(0)->after('Delivery_Details');
+            $table->unsignedInteger('Local_Funding')->default(0)->after('Short_Description');
             $table->string('Legacy_Delivery_Id')->nullable()->after('Local_Funding');
             $table->string('Legacy_Delivery_Method')->nullable()->after('Legacy_Delivery_Id');
 
